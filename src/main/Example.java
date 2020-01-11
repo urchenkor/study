a0009fe2388005758cfb6b5938cb50a7f17080e4
uzuraptor <phuckinganimals@gmail.com>
2020-01-11 19:04:49 +1000

    Второй коммит

--- src/main/Example.java
+++ src/main/Example.java
@@ -4,5 +4,6 @@ public class Example {
     //Example class
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("second commit");
     }
 }
